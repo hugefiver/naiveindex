1d4340163217a3f8135466492d5ffefa7c45469e
Hugefiver <wwyinhs@qq.com>
2018-09-04 16:37:46 +0800

    change edit list

--- hurry/admin.py
+++ hurry/admin.py
@@ -4,13 +4,15 @@ from .models import Post, PinPost
 
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['create_date', 'title',
+    list_display = ['title', 'create_date',
                     'change_date']
+    list_editable = ['title', 'create_date']
 
 
 class PinPostAdmin(admin.ModelAdmin):
-    list_display = ['pin', 'create_date', 'title',
-                    'change_date']
+    list_display = ['title', 'create_date',
+                    'pin', 'change_date']
+    list_editable = ['title', 'create_date']
 
 
 admin.site.register(Post, PostAdmin)
