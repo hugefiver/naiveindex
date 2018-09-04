15ffa2292a7a03ec6fc1098e9403ff962ae4ba71
Hugefiver <wwyinhs@qq.com>
2018-09-04 14:41:43 +0800

    add pymdownx

--- hurry/views.py
+++ hurry/views.py
@@ -8,15 +8,19 @@ from markdown_newtab import NewTabExtension
 def hide_page(request):
     pin_msg_list = PinPost.objects.all().order_by('-pin', '-create_date')
     msg_list = Post.objects.all().order_by('-create_date')
-    md_ex = [NewTabExtension(),
-             'pymdownx.inlinehilite',
-             'pymdownx.highlight',
-             'pymdownx.inlinehilite',
-             'pymdownx.extrarawhtml',
-             'pymdownx.tilde']
+    ex_md_ex = [NewTabExtension(),
+                'pymdownx.inlinehilite',
+                'pymdownx.highlight',
+                'pymdownx.inlinehilite',
+                'pymdownx.extrarawhtml',
+                'pymdownx.tilde',
+                'pymdownx.superfences',
+                'pymdownx.betterem',
+                'pymdownx.details',
+                'pymdownx.tasklist']
 
     for msg in pin_msg_list:
-        msg.main_text = markdown(msg.main_text, )
+        msg.main_text = markdown(msg.main_text, ex_md_ex)
     for msg in msg_list:
         msg.main_text = markdown(msg.main_text)
     return render(request, 'hidepage.html', context={'pin_msg_list': pin_msg_list,
