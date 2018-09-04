f8033a2d089a7a2b2c2da84dc655c1437ead88b3
Hugefiver <wwyinhs@qq.com>
2018-09-04 16:20:56 +0800

    add highlight suport

--- hurry/views.py
+++ hurry/views.py
@@ -20,9 +20,9 @@ def hide_page(request):
                 'pymdownx.tasklist']
 
     for msg in pin_msg_list:
-        msg.main_text = markdown(msg.main_text, ex_md_ex)
+        msg.main_text = markdown(msg.main_text, extensions=ex_md_ex)
     for msg in msg_list:
-        msg.main_text = markdown(msg.main_text)
+        msg.main_text = markdown(msg.main_text, extensions=ex_md_ex)
     return render(request, 'hidepage.html', context={'pin_msg_list': pin_msg_list,
                                                      'msg_list': msg_list})
 
